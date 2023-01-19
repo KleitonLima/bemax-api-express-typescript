4cf77012b33201ec3973e7d3875a701fb545365b
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-01-19 01:43:57 -0300

    feat(router): create & configure

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,12 @@
 import express, { Router, Request, Response } from "express";
 const app = express();
-const port = 3000;
 const route = Router();
 
 app.use(express.json());
-
-app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "Hello Wolrd" });
-});
-
 app.use(route);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "Hello World" });
 });
+
+export default app;
