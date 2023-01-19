bb35b0062a6ef62e68f01fb5239831964071fb0a
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-01-19 01:47:30 -0300

    feat: install cors & yours types & apply in app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express, { Router, Request, Response } from "express";
+import cors from 'cors'
+
 const app = express();
 const route = Router();
 
 app.use(express.json());
 app.use(route);
+app.use(cors())
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello World" });
