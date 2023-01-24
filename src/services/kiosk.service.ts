d227d64f3fbf5c5a52ca16b02d5cfd646dad4470
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-01-24 15:22:09 -0300

    feat(service): typing consts in kiosk.service

--- src/services/kiosk.service.ts
+++ src/services/kiosk.service.ts
@@ -3,19 +3,22 @@ import { Kiosk } from "../types";
 
 const KioskService = {
   getAll: async () => {
-    const kioks = await knex("kiosks");
-    return kioks;
-  },
-  getById: async (id: string) => {
-    const kioks = await knex("kiosks").where("id", id);
-    return kioks;
-  },
-  create: async (kiosk: Kiosk) => {
-    const kiosks = await knex("kioks").insert(kiosk);
+    const kiosks: Kiosk[] = await knex("kiosks");
     return kiosks;
   },
+
+  getById: async (id: string) => {
+    const kiosks: Kiosk = await knex("kiosks").where("id", id);
+    return kiosks;
+  },
+
+  create: async (kiosk: Kiosk) => {
+    const kiosks: Kiosk = await knex("kioks").insert(kiosk);
+    return kiosks;
+  },
+
   update: async (id: string, kiosk: Kiosk) => {
-    const kiosks = await knex("kiosks").where("id", id).update({
+    const kiosks: Kiosk = await knex("kiosks").where("id", id).update({
       name: kiosk.name,
       price: kiosk.price,
       quantity: kiosk.quantity,
@@ -24,9 +27,10 @@ const KioskService = {
     });
     return kiosks;
   },
+
   delete: async (id: string) => {
-    const kiosks = await knex("kiosks").where("id", id).del();
-    return kiosks;
+    const kiosk: Kiosk = await knex("kiosks").where("id", id).del();
+    return kiosk;
   },
 };
 
