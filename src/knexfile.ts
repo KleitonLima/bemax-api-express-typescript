885dd82c14be0cb9fd58f6633354a3cda9c097b1
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-01-23 22:44:24 -0300

    feat(knexfile): config connection database

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -1,7 +1,10 @@
-const pg = require("knex")({
-  client: "pg",
-  connection: process.env.PG_CONNECTION_STRING,
-  searchPath: ["knex", "public"],
-});
-
-export default pg
+export const knex = require("knex")({
+    client: "pg",
+    connection: {
+      host: "containers-us-west-46.railway.app",
+      port: 7335,
+      database: "railway",
+      user: "postgres",
+      password: "Y42wGvpL1vnfwrTiL77t",
+    },
+  });
